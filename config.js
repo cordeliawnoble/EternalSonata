8c90224cd65196ac70d1f392698c3d24d06ff33d
cordeliawnoble <cordelia.w.noble@gmail.com>
2026-09-14 15:51:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 window.ETERNAL_SONATA = {
   links: {
-    sonate: "",
+    sonate: "https://cordeliawnoble.github.io/Sonate/",
     pepiniere: "https://docs.google.com/spreadsheets/d/1oQtAt5mKygDJkHyDAPX6EXcuv5hfWaXyE4oBveUzqT4/edit",
-    passerelle: "",
+    passerelle: "https://cordeliawnoble.github.io/passerelle/",
     passeailes: "",
     nova: "",
-    todum: "",
-    drive: "",
-    site: "",
-    discord: "",
-    charte: "",
-    bureau: ""
+    todum: "https://cordeliawnoble.github.io/todum/",
+    site: "https://cordeliawnoble.fr/",
+    discord: "https://discord.gg/JhzFuwjsG",
   }
 };
