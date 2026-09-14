d9044171b6a8a119fd61a2621c97d070981fd331
cordeliawnoble <cordelia.w.noble@gmail.com>
2026-09-14 15:48:08 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+window.ETERNAL_SONATA = {
+  links: {
+    sonate: "",
+    pepiniere: "https://docs.google.com/spreadsheets/d/1oQtAt5mKygDJkHyDAPX6EXcuv5hfWaXyE4oBveUzqT4/edit",
+    passerelle: "",
+    passeailes: "",
+    nova: "",
+    todum: "",
+    drive: "",
+    site: "",
+    discord: "",
+    charte: "",
+    bureau: ""
+  }
+};
